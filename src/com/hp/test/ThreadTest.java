12b6a9ef5482b8892196cbaae18777f00751c30e
Kasvi-Technologies <kasvi.technologies@gmail.com>
2021-01-06 11:07:34 +0530

    Add files via upload

--- src/com/hp/test/ThreadTest.java
+++ src/com/hp/test/ThreadTest.java
@@ -2,15 +2,18 @@ package com.hp.test;
 
 import com.hp.threadsex.MyThread1;
 import com.hp.threadsex.MyThread2;
+import com.hp.threadsex.ThreadUtility;
 
 public class ThreadTest {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		MyThread1 myThread1 = new MyThread1();
+		ThreadUtility threadUtility = new ThreadUtility();
+		
+		MyThread1 myThread1 = new MyThread1(threadUtility);
 		myThread1.setName("First Thread");
-		MyThread2 myThread2 = new MyThread2();
+		MyThread2 myThread2 = new MyThread2(threadUtility);
 		myThread2.setName("Second Thread");
 		
 		myThread1.start();
